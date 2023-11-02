b041d1e496c40a324bb8a99b886d22d365f9741a
Shawn <svtrasmonte28@yahoo.com>
2023-11-02 14:33:19 -0500

    Edit to migration files

--- migrations/versions/20231101_122508_.py
+++ migrations/versions/20231101_122508_.py
@@ -8,6 +8,10 @@ Create Date: 2023-11-01 12:25:08.809907
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'd52005c956a1'
