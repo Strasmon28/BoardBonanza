33b52aa25218f25e68d6964f17b6f9171632bfa1
Shawn <svtrasmonte28@yahoo.com>
2023-11-07 17:13:31 -0600

    Added cover selection for update lists

--- react-app/src/components/UpdateListModal/index.js
+++ react-app/src/components/UpdateListModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { updateListThunk } from "../../store/lists";
+import "./UpdateListModal.css";
 
 function UpdateListModal({ list, boardId }) {
   const dispatch = useDispatch();
@@ -9,6 +10,7 @@ function UpdateListModal({ list, boardId }) {
   // const [user_id, setUser_id] = useState(0)
   const [title, setTitle] = useState(list?.title);
   const [cover, setCover] = useState(list?.cover);
+  const [selection, setSelection] = useState(0);
   const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
@@ -21,15 +23,31 @@ function UpdateListModal({ list, boardId }) {
     closeModal();
   };
 
+  const selectCover = (chosenCover, selectNum) => {
+    setCover(chosenCover);
+    setSelection(selectNum);
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <p>Cover</p>
-        <input
+        {/* <input
           type="text"
           value={cover}
           onChange={(e) => setCover(e.target.value)}
-        />
+        /> */}
+        <div className="cover-select-container">
+          <div className={selection === 1 ? "flower-select chosen" : "flower-select"} onClick={() => selectCover("flower", 1)}>
+          </div>
+
+          <div className={selection === 2 ? "starry-select chosen" : "starry-select"} onClick={() => selectCover("starry", 2)}>
+          </div>
+
+          {/* <div className={selection === 3 ? "grass-select chosen" : "grass-select"} onClick={() => selectCover("grass", 3)}>
+          </div> */}
+
+        </div>
 
         <p>Title</p>
         <input
